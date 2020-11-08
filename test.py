397b63fb7c0c893d62de5f3f6e61b5c9652a99d9
HsienYu <chenghsienyu@aim.com>
2020-11-08 21:50:19 +0800

    update

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ while True:
 
     GPIO.output(TRIG, False)  # Set TRIG as LOW
     print "Waiting For Sensor To Settle"
-    time.sleep(0.5)  # Delay of 2 seconds
+    time.sleep(2)  # Delay of 2 seconds
 
     GPIO.output(TRIG, True)  # Set TRIG as HIGH
     time.sleep(0.00001)  # Delay of 0.00001 seconds
